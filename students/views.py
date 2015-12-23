54ccaf5aca8be08c223c5f2e7415aef238c2356b
SergiySavarin <vir.host@gmail.com>
2015-12-23 15:47:55 +0200

    add loop for student list

--- students/views.py
+++ students/views.py
@@ -1,9 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Student's views
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Андрій',
+         'last_name': u'Корост',
+         'ticket': 2123,
+         'image': 'img/podoba3.jpg'},
+        {'id': 2,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/me.jpeg'},
+        {'id': 3,
+         'first_name': u'Тарас',
+         'last_name': u'Притула',
+         'ticket': 5332,
+         'image': 'img/piv.png'},
+        
+    )
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
