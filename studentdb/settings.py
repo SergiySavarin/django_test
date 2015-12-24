3513efb8142e01f7451b9b2ccc4112055846b20b
SergiySavarin <vir.host@gmail.com>
2015-12-24 10:06:33 +0200

    Add portal url, group.html and dynamic urls for bookmarks

--- studentdb/settings.py
+++ studentdb/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+from django.conf import global_settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PORTAL_URL = 'http://localhost:8000'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -64,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'studentdb.context_processors.students_proc',
             ],
         },
     },
--- students/views.py
+++ students/views.py
@@ -37,7 +37,16 @@ def students_delete(request, sid):
 
 # Gropu's views
 def groups_list(request):
-    return HttpResponse('<h1>Groups Listing</h1>')
+    groups = (
+        {'id': 1,
+         'name': u'Мтм-1',
+         'leader': {'id': 1, 'name': u'Тарас Мельник'}},
+        {'id': 2,
+         'name': u'Мтм-22',
+         'leader': {'id': 2, 'name': u'Микола Садовський'}},
+    )
+    return render(request, 'students/groups_list.html',
+        {'groups': groups})
 
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
